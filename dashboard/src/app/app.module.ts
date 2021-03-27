9327ab89724129d66dcc60e67d16814972ae6021
lizhao <72632137+lizhaoliu97@users.noreply.github.com>
2021-03-27 18:19:35 +0000

    add dockerfile

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { HeadPageComponent } from './head-page/head-page.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarChartComponent,
-    HeadPageComponent
+    HeadPageComponent,
+    DoughnutChartComponent,
+    RadarChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
